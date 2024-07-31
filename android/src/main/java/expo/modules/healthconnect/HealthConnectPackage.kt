08698f9f311ef38c7baed4df03de9d581a7d2243
Matin Zadeh Dolatabad <24797481+matinzd@users.noreply.github.com>
2024-07-31 19:00:34 +0200

    fix: expo dev client inteference issue with activity handelrs (#2)

--- android/src/main/java/expo/modules/healthconnect/HealthConnectPackage.kt
+++ android/src/main/java/expo/modules/healthconnect/HealthConnectPackage.kt
@@ -3,9 +3,10 @@ package expo.modules.healthconnect
 import android.content.Context
 import expo.modules.core.interfaces.Package
 import expo.modules.core.interfaces.ReactActivityHandler
+import expo.modules.core.interfaces.ReactActivityLifecycleListener
 
 class HealthConnectPackage: Package {
-    override fun createReactActivityHandlers(activityContext: Context?): MutableList<out ReactActivityHandler> {
-        return mutableListOf(HeatlhConnectPermissionReactActivityHandler())
+    override fun createReactActivityLifecycleListeners(activityContext: Context?): MutableList<out ReactActivityLifecycleListener> {
+        return mutableListOf(HealthConnectPermissionReactActivityHandler())
     }
 }
